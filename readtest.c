a0500c214a241f207158efd0078105a5f1e2c2d6
Augustus Wynn <wyna@seas.upenn.edu>
2016-06-20 14:38:45 -0400

    started to calc

--- readtest.c
+++ readtest.c
@@ -21,6 +21,7 @@ unsigned char ham(unsigned char c1, unsigned char c2){
 
 
 int main(int argc, char **argv){
+  /*
   char* test = "cd4e49485429ef53d3f91e9ec8c3c85a094405da";
   unsigned char val[20];
   int count;
@@ -29,6 +30,20 @@ int main(int argc, char **argv){
     printf("NEW VAL:%u\n", (unsigned int) val[count]);
     printf("hex check:%02x\n\n", val[count]);
    }
+*/
 
+
+  FILE *hashes;
+  char line[50]; //will only be commits, 40 hex characters
+  hashes = fopen("hashes.txt", "r");
+  while (fgets(line, 100, hashes) != NULL){
+    int count;
+    unsigned char val[20];
+    printf("%s, Vals: ", line);
+    for (count = 0; count < 20; count++){
+      sscanf(line + 2*count, "%02x", &val[count]);
+      printf("%u ", (unsigned int) val[count]);
+     }
+  }
 }
 
